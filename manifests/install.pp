c135b039cfd5326982e9060f5f204e9f7d5222bb
Yorick Poels <yorick.poels@kuleuven.be>
2023-01-11 18:52:42 +0100

    Fix install

--- manifests/install.pp
+++ manifests/install.pp
@@ -8,7 +8,7 @@ class crowdsec::install (
 ) {
 # install package requirements
   if $crowdsec::manage_packages == true {
-    $crowdsec::required_packages.each |$required_package | {
+    $crowdsec::required_packages.each |$crowdsec::required_package | {
       ensure_packages($crowdsec::required_package, { ensure => $crowdsec::required_packages_ensure })
     }
   }
