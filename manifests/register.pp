4e11a26fbfa6bdda5be97822968852eaddd8877d
Yorick Poels <yorick.poels@gmail.com>
2024-12-20 15:09:40 +0100

    Fix register facts

--- manifests/register.pp
+++ manifests/register.pp
@@ -7,18 +7,18 @@
 class crowdsec::register {
 # Check if agent is already registered
 exec { 'check_agent_registration':
-  command     => 'cscli machines list -o raw | grep ${facts.networking.ip} | grep true || echo "Not found"',
+  command     => 'cscli machines list -o raw | grep $facts['networking']['ip'] | grep true || echo "Not found"',
   path        => ['/bin', '/usr/bin'],
   logoutput   => true,
   refreshonly => true,
   notify      => Exec['register_and_validate_agent'],
-  onlyif      => "test -z \"$(cscli machines list -o raw | grep ${facts.networking.ip} | grep true)\"",
+  onlyif      => "test -z \"$(cscli machines list -o raw | grep $facts['networking']['ip'] | grep true)\"",
   subscribe   => Exec['flush_handlers'],
 }
 
 # Register and validate agent
 exec { 'register_and_validate_agent':
-  command     => "cscli lapi register -u ${crowdsec_lapi_url} --machine ${facts.networking.fqdn} && cscli machines validate ${facts.networking.fqdn}",
+  command     => "cscli lapi register -u ${crowdsec_lapi_url} --machine $facts['networking']['fqdn'] && cscli machines validate $facts['networking']['fqdn']",
   path        => ['/bin', '/usr/bin'],
   logoutput   => true,
   refreshonly => true,
