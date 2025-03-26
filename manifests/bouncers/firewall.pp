c10e8e3a91d6dac89b34bb2a2eb813ba3aeea615
Yorick Poels <yorick.poels@kuleuven.be>
2025-03-26 16:29:47 +0100

    Fix firewall bouncer

--- manifests/bouncers/firewall.pp
+++ manifests/bouncers/firewall.pp
@@ -1,13 +1,10 @@
-@summary A short summary of the purpose of this class
+# @summary A short summary of the purpose of this class
 #
 # A description of what this class does
 #
-# @example
-#   include crowdsec::firewall
 class crowdsec::bouncers::firewall {
-
   package { 'crowdsec-firewall-bouncer':
     ensure          => present,
-    install_options => '--enablerepo=crowdsec'
+    install_options => '--enablerepo=crowdsec',
   }
 }
